faae7b801134573c1737fa7355652e268cab01a3
Tobias Årud <me@tobiasarud.se>
2016-05-12 19:52:51 +0200

    db_connect change

--- inc/db_connect.php
+++ inc/db_connect.php
@@ -1,18 +1,10 @@
 <?php
-/*
-$dsn = 'mysql:host=localhost;dbname=blog_db';
+$dsn = "mysql:host=localhost;dbname=blog_db";
 // $dsn = "mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;dbname=blog_db;";
-$username = 'root';
-$password = '';
-// $password = 'root';
-$options = array(
-    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-); 
-
-$db = new PDO($dsn, $username, $password, $options);
+$user = "root";
+$password = "";
+// $password = "root";
+$db = new PDO($dsn, $user, $password);
+$db->exec("set names utf8");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-*/
-$db = new PDO("mysql:host=localhost;dbname=blog_db;", "root", "");
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 ?>
